2db1f0231a8a7cc1ec6735d5a6839865ef4cff74
sriramtalasila <sriramprasadtalasila@gmail.com>
2018-12-23 08:58:27 +0530

    completed and stopped due to rejected

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ const {
 
 } = require('actions-on-google');
 
+const { WebhookClient } = require('dialogflow-fulfillment');
+const { Card, Suggestion } = require('dialogflow-fulfillment');
 const admin = require('firebase-admin');
+
 admin.initializeApp({
     credential: admin.credential.cert({
         "project_id": "valiant-39346",
@@ -155,6 +158,43 @@ app.intent('Eventcoordinator', (conv, params) => {
 });
 
 
-expressApp.post('/fulfillment', app)
+expressApp.post('/fulfillment', (request, response) => {
+    const agent = new WebhookClient({ request, response });
+    console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+    console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+
+    function welcome(agent) {
+        agent.add(`Welcome to my agent!`);
+    }
+
+    function fallback(agent) {
+        agent.add(`I didn't understand`);
+        agent.add(`I'm sorry, can you try again?`);
+    }
+
+
+    function yourFunctionHandler(agent) {
+        agent.add(`This message is from Dialogflow's Cloud Functions for Firebase inline editor!`);
+        agent.add(new Card({
+            title: `Title: this is a card title`,
+            imageUrl: 'https://dialogflow.com/images/api_home_laptop.svg',
+            text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
+            buttonText: 'This is a button',
+            buttonUrl: 'https://docs.dialogflow.com/'
+        })
+        );
+        agent.add(new Suggestion(`Quick Reply`));
+        agent.add(new Suggestion(`Suggestion`));
+        agent.setContext({ name: 'weather', lifespan: 2, parameters: { city: 'Rome' } });
+    }
+
+
+    let intentMap = new Map();
+    intentMap.set('Default Welcome Intent', welcome);
+    intentMap.set('Default Fallback Intent', fallback);
+    // intentMap.set('<INTENT_NAME_HERE>', yourFunctionHandler);
+    // intentMap.set('<INTENT_NAME_HERE>', googleAssistantHandler);
+    agent.handleRequest(intentMap);
+});
 
 expressApp.listen(PORT, () => console.log(`Listening on ${PORT}`))
